cc0bd4517d39fc85ee22d0d3c317755512131ecc
Andre Winkler <gluehloch@googlemail.com>
2023-12-08 16:56:35 +0100

    Function expression vs declaration...

--- main.js
+++ main.js
@@ -1,20 +1,58 @@
 
+class Person {
+    name;
+    firstName;
+    age;
+
+    constructor(name, firstName, age) {
+        this.name = name;
+        this.firstName = firstName;
+        this.age = age;
+    }
+
+    print() {
+        return this.fullName + ', ' + this.age;
+    }
+
+    get fullName() {
+        return this.name + ' ' + this.firstName;
+    }
+
+    isUnderEighteen = () => {
+        return this.age < 18;
+    }
+
+    isOlderThanEighteen = function()   {
+        return this.age >= 18;
+    }
+
+}
+
+const person1 = new Person('Doe', 'John', 25);
+const person2 = new Person('Doe', 'Jane', 22);
+const person3 = new Person('Doe', 'Joe', 18);
+const person4 = new Person('Doe', 'Jack', 15);
+
+console.log(person1.print());
+console.log(person2.print());
+console.log(person3.print());
+console.log(person4.print());
+console.log(person4.isUnderEighteen(), person4);
+console.log(person3.isUnderEighteen(), { person3 });
+console.log(person3.isOlderThanEighteen(), person3);
+
 const myClass = {
     name: 'MyClass',
-    sayHello: () => {
+    sayHello: function() {
         return 'Hello ' + this.name;
     },
-    sayHallo2: () => { 
-        return 'Hello ' + name;
-    },
-    sayHallo3: function() { 
+    sayHello2: () => { 
         return 'Hello ' + this.name;
     }
 };
 
 console.log(myClass.sayHello());
-console.log(myClass.sayHello2());
-console.log(myClass.sayHello3());
+console.log(myClass.sayHello2()); // this.name is undefined
 
 const x = 100;
 console.log('x = ' + x);
@@ -23,5 +61,5 @@ const items = ['item1', 'item2', 'item3'];
 console.log('iteams = ' + items);
 console.log(items);
 
-console.trace(items);
+// console.trace(items);
 
