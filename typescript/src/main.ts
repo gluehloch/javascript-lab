71559f2d16ddfe523c8e486d6c17b86bad9acc6c
Andre Winkler <gluehloch@googlemail.com>
2023-12-09 20:15:15 +0100

    Typescript enum/string type.

--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -1,2 +1,10 @@
+type status = 'FINAL' | 'CONSTRUCTION' | 'TEST';
+
+let myStatus: status = 'FINAL';
+myStatus = 'CONSTRUCTION';
+
+console.log(myStatus);
+const x = 0;
+
 const a = 100;
 console.log(a);
